334b351f6eea8c52e8a00206dda027e08ece7042
Muhammed Nasser <91643116+Mu7ammedNasser@users.noreply.github.com>
2024-05-16 12:19:53 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var qoutelist = [
+  `"Resentment is like drinking poison and waiting for your enemies to die."`,
+  `"You miss 100% of the shots you don't take."`,
+  `“Be yourself; everyone else is already taken.”`,
+  `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”`,
+  `“A room without books is like a body without a soul.”`,
+  `“If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.”`,
+];
+var qoutelistAouther = [
+  `--Nelson Mandela`,
+  `--Wayne Gretzy`,
+  `― Oscar Wilde`,
+  `― Albert Einstein`,
+  `― Marcus Tullius Cicero`,
+  `― J.K. Rowling, Harry Potter and the Goblet of Fire`,
+];
+
+var lastRandomNuber = -1;
+function generateQuote() {
+  var x = Math.floor(Math.random() * qoutelist.length);
+  while (x === lastRandomNuber) {
+    x = Math.floor(Math.random() * qoutelist.length);
+  }
+  previousIndex = x;
+  document.getElementById("quote").innerHTML = qoutelist[x];
+  document.getElementById("quoteAouther").innerHTML = qoutelistAouther[x];
+}
